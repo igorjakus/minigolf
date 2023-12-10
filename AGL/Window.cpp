abd9a29e97ee226ad062b5846ca14ae3b19a48f6
frogrammer9 <oskar@meler.pl>
2023-12-10 23:38:15 +0100

    changed code styling

--- AGL/Window.cpp
+++ AGL/Window.cpp
@@ -156,9 +156,7 @@ bool GL::Window::GetButtonPressed(int key)
 glm::dvec2 GL::Window::GetMousePos()
 {
 	if (m_ID == nullptr)
-	{
-		DTL_ERR("Window hasn't yet been created. First create a window before trying to get mouse position."); return {0, 0};
-	}
+	{ DTL_ERR("Window hasn't yet been created. First create a window before trying to get mouse position."); return {0, 0}; }
 	glm::dvec2 mPos;
 	glfwGetCursorPos(m_ID, &mPos.x, &mPos.y);
 	return mPos;
