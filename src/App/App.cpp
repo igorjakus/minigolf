39ea84cc1512e8b36a2bc746a963f96b1b55d751
frogrammer9 <oskar@meler.pl>
2023-12-16 02:22:20 +0100

    Introductory implemented ImGui !THIS VERTSION IS HIGHLY UNSTABLE!

--- src/App/App.cpp
+++ src/App/App.cpp
@@ -3,7 +3,9 @@
 #include<Window.h>
 #include<Shader.h>
 #include"Agl.h"
-
+#include<ImGui/imgui.h>
+#include<ImGui/imgui_impl_glfw.h>
+#include<ImGui/imgui_impl_opengl3.h>
 
 int main() 
 {
@@ -13,19 +15,42 @@ int main()
 #endif
 
 	agl::Init();
+	/*temp*/
+	ImGui::CreateContext();
+	IMGUI_CHECKVERSION();
+	/*/temp*/
 	agl::Window win1(640, 480, "minigolf");
 
+
 	win1.create();
+
+	/*temp*/
+	ImGui_ImplGlfw_InitForOpenGL(win1.passPointer(), true);
+	ImGui_ImplOpenGL3_Init();
+	/*/temp*/
+
 	win1.setIcon("assets/icon/icon.png", "assets/icon/icon.png");
 	win1.setWindowPos(640, 300);
 
 
 	while (!win1.closeCallBack())
 	{
-		win1.IsKeyPressed(12);
 
+		/*temp*/
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::ShowDemoWindow();
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		/*/temp*/
 		win1.FEP();
 	}
-	
+	/*temp*/
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+	/*/temp*/
 	return 0;
 }
