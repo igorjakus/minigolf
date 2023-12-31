a2a2345a04f690d99cb87cc006624a9886765dba
GoscZnickiem <maciek.6098@gmail.com>
2023-12-31 21:33:54 +0100

    Szkielet klasy Input

--- src/App/UserInput.cpp
+++ src/App/UserInput.cpp
@@ -4,20 +4,82 @@
 namespace golf {
 
 
+bool Input::isKeyPressed(std::string key) {
+
+}
+
+bool Input::isKeyClicked(std::string key) {
+
+}
+
+float Input::getMouseX() {
+
+}
+
+float Input::getMouseY() {
+
+}
+
+bool Input::isLeftMousePressed() {
+
+}
+
+bool Input::isLeftMouseClicked() {
+
+}
+
+bool Input::isRightMousePressed() {
+
+}
+
+bool Input::isRightMouseClicked() {
+
+}
+
+float Input::getWheelOffset() {
+
+}
+
+bool Input::isFocused() {
+
+}
+
 void Input::setTargetWindow(const agl::Window& window) {
 	m_window = window.passPointer();
-	
-	if(glfwGetWindowUserPointer(m_window) != nullptr) {
-		DTL_ERR("User pointer już ustawiony!");
-		return;
-	}
-	glfwSetWindowUserPointer(m_window, this);
 
-	// glfwSetKeyCallback(m_window, keyCallback);
+	glfwSetKeyCallback(m_window, s_keyCallback); 
+	glfwSetCursorPosCallback(m_window, s_mousePosCallback);
+	glfwSetMouseButtonCallback(m_window, s_mouseCallback);
+	glfwSetScrollCallback(m_window, s_scrollCallback);
+	glfwSetWindowFocusCallback(m_window, s_focusCallback);
 }
 
-void Input::keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) { //NOLINT
+void Input::s_keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) { //NOLINT
+	// s_instance->doSomething();
+}
+
+void Input::s_mousePosCallback(GLFWwindow* window, double xPos, double yPos) { //NOLINT
 
 }
+void Input::s_mouseCallback(GLFWwindow* window, int buttom, int action, int mods) { //NOLINT
+
+}
+void Input::s_scrollCallback(GLFWwindow* window, double	xOffset, double yOffset) { //NOLINT
+
+}
+void Input::s_focusCallback(GLFWwindow* window, int focused) { //NOLINT
+
+}
+
+
+Input::Input() 
+	:m_window(nullptr) {
+	s_instance = this;
+}
+
+Input::~Input() {
+	s_instance = nullptr;
+}
+
 
 }
--- src/App/UserInput.h
+++ src/App/UserInput.h
@@ -3,21 +3,53 @@
 // Wrapper class to access glfw information about user's input.
 
 #include "Window.h"
+#include <string>
 
 namespace golf {
 
 
 class Input {
+
 public:
+
+	~Input();
+	Input(const Input &) = delete;
+	Input(Input &&) = delete;
+	Input &operator=(const Input &) = delete;
+	Input &operator=(Input &&) = delete;
+
+	bool isKeyPressed(std::string key);
+	bool isKeyClicked(std::string key);
+
+	float getMouseX();
+	float getMouseY();
+	bool isLeftMousePressed();
+	bool isLeftMouseClicked();
+	bool isRightMousePressed();
+	bool isRightMouseClicked();
+
+	float getWheelOffset();
+
+	bool isFocused();
 
 	void setTargetWindow(const agl::Window& window);
 
-	void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-
 private:
+	Input();
 
 	GLFWwindow* m_window;
-	
+
+	// static constexpr size_t keysNum = 130;
+	// std::array<bool, keysNum> keys;
+
+	static Input* s_instance; //NOLINT
+	static void s_keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+	static void s_mousePosCallback(GLFWwindow* window, double xPos, double yPos);
+	static void s_mouseCallback(GLFWwindow* window, int buttom, int action, int mods);
+	static void s_scrollCallback(GLFWwindow* window, double	xOffset, double yOffset);
+	static void s_focusCallback(GLFWwindow* window, int focused);
+
+	friend class AppData;
 };
 
 
