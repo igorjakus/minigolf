c4a860fde1004365a7d9fb7a6503d3ab361eb7fe
GoscZnickiem <maciek.6098@gmail.com>
2024-01-02 17:10:04 +0100

    Poprawiono czytelność maina

--- src/App/main.cpp
+++ src/App/main.cpp
@@ -2,14 +2,14 @@
 
 #include "App.h"
 
-static const bool cleanupRegister = (atexit(agl::Terminate) == 0);
-
 int main() {
 
 #ifdef __DIST__
 	dtl::Log.settings(DTL_DEFAULT, DTL_DEFAULT, DTL_DEFAULT, DTL_DEFAULT, DTL_PROGRAM_TIME, DTL_FILE);
 	dtl::Log.setFile("Log_error.txt");
 #endif
+
+	atexit(agl::Terminate);
 
 	agl::Init();
 
