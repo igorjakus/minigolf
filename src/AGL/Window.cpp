18e07703bf7e321fd2685598f645d82fcacf4ae5
Igor Jakus <igorjakus@protonmail.com>
2024-01-16 19:41:37 +0100

    Fix opening window error on MacOS

--- src/AGL/Window.cpp
+++ src/AGL/Window.cpp
@@ -14,6 +14,12 @@ agl::Window::~Window()
 void agl::Window::create()
 {
 	m_monitor = glfwGetPrimaryMonitor();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	glfwWindowHint(GLFW_SAMPLES, 4);
 	m_ID = glfwCreateWindow(m_winSizeW, m_winSizeH, m_title.c_str(), NULL, NULL);
 	if (!m_ID)
