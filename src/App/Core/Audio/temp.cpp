8d66425a177fc5c03612036fbda0ecd8c8bba4c8
Igor Jakus <igorjakus@protonmail.com>
2024-01-17 22:41:16 +0100

    Add comments describing audio engine

--- src/App/Core/Audio/temp.cpp
+++ src/App/Core/Audio/temp.cpp
@@ -13,7 +13,7 @@ void init_engine(ma_engine* engine) {
 
     // Failed to initialize the engine
     if (result != MA_SUCCESS)
-        std::cerr << "Failed to initialize the engine!";
+        std::cerr << "Failed to initialize the engine!"; // DTL log not ok
     // DTL log ok
 }
 
@@ -22,7 +22,7 @@ void load_sound(ma_sound* sound, std::string soundFilePath, ma_engine* engine) {
     ma_result result;
     result = ma_sound_init_from_file(engine, soundFilePath.c_str(), 0, NULL, NULL, sound);
     if (result != MA_SUCCESS)
-        std::cerr << "Failed to initialize the sound!";
+        std::cerr << "Failed to initialize the sound!"; // DTL log not ok
     // DTL log ok
 }
 
@@ -41,6 +41,16 @@ void temp()
     std::string soundFilePath = "assets/audio/neon.wav";
     load_sound(&neon, soundFilePath, &engine);
 
+    /* cały dźwięk będzie na osobnym wątku ale nie wiem jeszcze 
+       jak rozwiązać problem ze zmianą głośności w czasie trwania programu
+       być może po przy byciu w ustawieniach wątek będzie zatrzymywany
+       a następnie wznawiany przy wyjściu z ustawień,
+       już z odpowiednimi ustawieniami
+    */ 
     std::thread SoundThread(playSound, &engine, soundFilePath);
+    
+    // jak testowałem odpalanie dźwięku to bez getchara od razu się kończy
+    // gdzie w dokumentacji nie jest to w żaden sposob rozwiniete
+    // a w podanych przykladach autor wykorzystuje getchara
     getchar();
 }
